57676ea5772ca8930dddd0235b1fedba2f3b0218
Greg Loughmiller <gloughmiller@yahoo.com>
2017-12-22 10:38:20 -0500

    Update linux.pp

--- site/profile/manifests/app/sample_website/linux.pp
+++ site/profile/manifests/app/sample_website/linux.pp
@@ -1,6 +1,6 @@
 class profile::app::sample_website::linux (
   String $doc_root = '/var/www/html',
-  Integer $webserver_port = 8080,
+  Integer $webserver_port = 8787,
   String $website_source_dir = 'puppet:///modules/profile/app/sample_website'
 ) {
 
